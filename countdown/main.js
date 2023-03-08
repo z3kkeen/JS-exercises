c348f589c42fb3dafe00cd81e6b59bea971ce421
z3keen <alma@henricson.fi>
2023-03-08 13:27:10 +0200

    fixes

--- countdown/main.js
+++ countdown/main.js
@@ -4,9 +4,12 @@ let time = 10
 let timer = setInterval(function(){
   if(time <= 0){
     clearInterval(timer);
-    document.getElementById("display").innerHTML = "Times up!"
+    document.getElementById("display").innerHTML = "Hej på dig!"
+    document.getElementById("display").classList.add("red")
+  } else if (time > 1) {
+    document.getElementById("display").innerHTML = time + " sekunder"
   } else {
-    document.getElementById("display").innerHTML = time + " seconds remaining"
+    document.getElementById("display").innerHTML = time + " sekund"
   }
   time = time - 1;
 }, 1000);
